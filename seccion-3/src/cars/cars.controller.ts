dfcba033b5cc6f517a46381bc293573ddc586e71
Santiago Sierra Cano <santiscano@gmail.com>
2024-01-11 12:59:49 -0500

    section 3

--- seccion-3/src/cars/cars.controller.ts
+++ seccion-3/src/cars/cars.controller.ts
@@ -1,26 +1,25 @@
 import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post } from '@nestjs/common';
 import { CarsService } from './cars.service';
 
-@Controller('cars') // ruta principal de todos los metodos
+@Controller('cars')
 export class CarsController {
   
   constructor(
-    private readonly carsService: CarsService //inyecion de dependencias es decir instancia la clase del servicio
+    private readonly carsService: CarsService
   ) {}
 
-  @Get() // decorador que define el tipo de peticion y ruta
+  @Get()
   getAllCars() {
     return this.carsService.findAll()
   }
 
   @Get(':id')
   getCarById( @Param('id', ParseIntPipe ) id: number ) {
-    // @Param recibe el parametro y tambien lo parsea a numero
     return this.carsService.findOneById( id );
   }
 
   @Post()
-  createCar( @Body() body: any ) { // extrae el body de la solicitud
+  createCar( @Body() body: any ) {
     return body;
   }
 
