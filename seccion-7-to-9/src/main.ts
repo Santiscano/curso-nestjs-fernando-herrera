e3e8163ffe88a0f7a9b7727d4ea16d2269121359
Santiago Sierra Cano <santiscano@gmail.com>
2024-02-05 19:20:25 -0500

    section-7-to-9

---
+++ seccion-7-to-9/src/main.ts
@@ -0,0 +1,26 @@
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  
+  const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api/v2');
+
+  app.useGlobalPipes( // es un pipe global de validaciones
+    new ValidationPipe({
+      whitelist: true, // elimina la data que no espero, solo deja la esperada
+      forbidNonWhitelisted: true, // genera el error si envia datos que no espero
+      transform: true, // permite la transformacion de los DTO para los que recibe como parametros o body
+      transformOptions: {
+        enableImplicitConversion: true,
+      }
+    })
+  );
+
+
+  await app.listen( process.env.PORT );
+  console.log(`App running on port ${ process.env.PORT }`)
+}
+bootstrap();
